82a43dac0f5a29454a24b83ad6a269e10c4adb53
John Ngugi <johnngugi777@gmail.com>
2019-08-14 19:40:31 +0300

    Add solution

--- bank-account/src/main/kotlin/BankAccount.kt
+++ bank-account/src/main/kotlin/BankAccount.kt
@@ -1,5 +1,24 @@
 class BankAccount {
 
+    private var open: Boolean = true
     var balance: Int = 0
+        get() {
+            if (!open) {
+                throw IllegalStateException()
+            }
+            return field
+        }
+
+    fun adjustBalance(amount: Int) {
+        synchronized(this) {
+            balance += amount
+        }
+    }
+
+    fun close() {
+        synchronized(this) {
+            open = false
+        }
+    }
 
 }
